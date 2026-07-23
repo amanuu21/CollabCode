945e16643ec0d9f7cc464988236207c28043d3b6
Amanuel Wondimu <amanuelabb@gmail.com>
2026-07-23 11:02:06 +0300

    chore: configure automatic reconnect backoff with jitter (iter 2)

--- frontend/src/services/socketClient.ts
+++ frontend/src/services/socketClient.ts
@@ -1,3 +1,6 @@
 
 # Progress update on 2026-07-13 - chore: configure automatic reconnect backoff with jitter
 # Revision: 10
+
+# Progress update on 2026-07-23 - chore: configure automatic reconnect backoff with jitter (iter 2)
+# Revision: 20
